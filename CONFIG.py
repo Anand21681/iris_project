6263af1b72fc602848d45a7a05c9512e345d9d8f
Anand <andikakad@gmail.com>
2023-03-14 16:51:09 +0530

    changes in config path

--- CONFIG.py
+++ CONFIG.py
@@ -1,5 +1,5 @@
-MODEL_PATH=r"artifacts\model.model.pkl"
-ASSET_PATH=r"artifacts\asset.asset.json"
+MODEL_PATH=r"artifacts/model.pkl"
+ASSET_PATH=r"artifacts/asset.json"
 
-PORT_NUMBER=8000
+PORT_NUMBER = 8000
 HOST_NAME="0.0.0.0"
